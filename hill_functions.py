09325acd4273c28469e34b7fe4ea8505843d0391
himoto <himoto@protein.osaka-u.ac.jp>
2019-07-24 20:21:06 +0900

    Update hill_function.py

--- hill_functions.py
+++ hill_functions.py
@@ -13,9 +13,25 @@ class HillFunctions(object):
         return bottom + (top-bottom)*x**n/(K**n+x**n)
 
     def get_param(self):
+        min_data = np.amin(self.y_data)
+        max_data = np.amax(self.y_data)
+        h = abs(max_data - min_data)
+        param_initial = [
+            max_data,
+            min_data,
+            0.5*(self.x_data[-1]-self.x_data[0]),
+            1
+        ]
+        param_bounds = (
+            [max_data-0.01*h,min_data-0.01*h,0,0.01],
+            [max_data+0.01*h,min_data+0.01*h,self.x_data[-1],1000]
+        )
+        
         popt,pcov = curve_fit(
             self.equation,self.x_data,self.y_data,
-            bounds=(0.0,float('inf')),maxfev=32767
+            p0 = param_initial,
+            bounds = param_bounds,
+            maxfev = 32767
         )
 
         return popt
@@ -23,15 +39,15 @@ class HillFunctions(object):
     def fitting(self,x_fit):
         popt = self.get_param()
         print(
-            '%s = %e\n%s = %e\n%s = %e\n%s = %e'\
-            %('top',popt[0],'bottom',popt[1],'K',popt[2],'n',popt[3])
+            '%s = %e\n%s = %e'\
+            %('EC50',popt[2],'Hill coefficient',popt[3])
         )
 
-        return self.equation(x_fit,popt[0],popt[1],popt[2],popt[3])
+        return self.equation(x_fit,popt[0],popt[1],popt[2],popt[3]),popt[2]
 
     def display(self):
         x_fit = np.linspace(self.x_data[0],self.x_data[-1],100)
-        y_fit = self.fitting(x_fit)
+        y_fit,EC50 = self.fitting(x_fit)
 
         plt.figure(figsize=(9,6))
         plt.rcParams['font.size'] = 20
@@ -44,6 +60,7 @@ class HillFunctions(object):
 
         plt.xscale('log')
         plt.xlim(self.x_data[0],self.x_data[-1])
+        plt.xticks([self.x_data[0],EC50,self.x_data[-1]])
         plt.xlabel('x')
         plt.ylabel('y')
         plt.legend(loc='upper left',frameon=False)
