e8d39b8a3600cff0de26fcec8d04369f88b616fa
himoto <31299606+himoto@users.noreply.github.com>
2022-09-14 14:24:58 +0100

    Create UserDefined

---
+++ hillfit/experimental/user_defined.py
@@ -0,0 +1,177 @@
+import os
+import re
+from typing import Callable, List, Optional, Union
+
+import numpy as np
+from matplotlib import pyplot as plt
+from pandas import DataFrame
+from scipy.optimize import curve_fit
+from sigfig import round
+from sklearn.metrics import r2_score
+
+
+class UserDefined(object):
+    def __init__(
+        self,
+        x_data: Union[List[float], np.ndarray],
+        y_data: Union[List[float], np.ndarray],
+        equation : Callable,
+    ) -> None:
+        """Fitting user-defined function to Experimental Data.
+
+        Parameters
+        ----------
+        x_data, y_data : Union[List[float], np.ndarray]
+            specifies the x-values & y-values, respectively, of the raw data that will be fitted
+            with the Hill equation.
+        equation : Callable
+            User defined function to be fitted.
+        """
+        self.x_data = np.array(x_data) if isinstance(x_data, list) else x_data
+        self.y_data = np.array(y_data) if isinstance(y_data, list) else y_data
+        self._equation = equation
+        if self.x_data[0] > self.x_data[-1]:
+            raise ValueError(
+                f"The first point {self.x_data[0]} and the last point {self.x_data[-1]} are not amenable with the scipy.curvefit function."
+            )
+
+    def _get_param(self, curve_fit_kws: dict) -> List[float]:
+        popt, _ = curve_fit(self._equation, self.x_data, self.y_data, **curve_fit_kws)
+        return [float(param) for param in popt]
+
+    def _regression(
+        self,
+        x_fit,
+        y_fit,
+        x_label,
+        y_label,
+        title,
+        sigfigs,
+        log_x,
+        print_r_sqr,
+        generate_figure,
+        view_figure,
+        *params,
+    ) -> None:
+        corrected_y_data = self._equation(self.x_data, *params)
+        self.r_2 = r2_score(self.y_data, corrected_y_data)
+
+        # define the regression plot
+        if generate_figure:
+            plt.rcParams["figure.figsize"] = (11, 7)
+            plt.rcParams["figure.dpi"] = 150
+            self.figure, self.ax = plt.subplots()
+            self.ax.plot(x_fit, y_fit, label="Hill fit")
+            self.ax.scatter(self.x_data, self.y_data, label="raw_data")
+            if log_x:
+                self.ax.set_xscale("log")
+            self.ax.set_xlabel(x_label)
+            self.ax.set_ylabel(y_label)
+            self.ax.set_title(title)
+            self.ax.legend(loc="lower right")
+            if print_r_sqr:
+                # define the coordinates location of the printed R^2 on the figure
+                y_coordinate = 0.7 * y_fit[-1]
+                if y_coordinate < y_fit[0]:
+                    y_coordinate = 1 * y_fit[0]
+                x_coordinate = 0.8 * x_fit[-1]
+                if x_coordinate < x_fit[0]:
+                    x_coordinate = 2 * x_fit[0]
+
+                self.ax.text(
+                    x_coordinate,
+                    y_coordinate,
+                    "R\N{superscript two}: " + f"{round(self.r_2, sigfigs)}",
+                )
+
+            if view_figure:
+                self.figure.show()
+
+    def fitting(
+        self,
+        x_label: str = "x",
+        y_label: str = "y",
+        title: str = "User-defined function",
+        sigfigs: int = 6,
+        log_x: bool = False,
+        print_r_sqr: bool = True,
+        generate_figure: bool = True,
+        view_figure: bool = True,
+        export: bool = True,
+        export_directory: Optional[str] = None,
+        export_name: Optional[str] = None,
+        curve_fit_kws: Optional[dict] = None,
+    ):
+        if curve_fit_kws is None:
+            curve_fit_kws = {}
+        self.generate_figure = generate_figure
+        self.x_fit = np.logspace(
+            np.log10(self.x_data[0]), np.log10(self.x_data[-1]), len(self.y_data)
+        )
+        params = self._get_param(curve_fit_kws)
+        self.y_fit = self._equation(self.x_fit, *params)
+
+        self._regression(
+            self.x_fit,
+            self.y_fit,
+            x_label,
+            y_label,
+            title,
+            sigfigs,
+            log_x,
+            print_r_sqr,
+            generate_figure,
+            view_figure,
+            *params,
+        )
+        if export:
+            self.export(params, export_directory, export_name, generate_figure)
+
+    def export(
+        self,
+        params: List[float],
+        export_directory: Optional[str] = None,
+        export_name: Optional[str] = None,
+        figure=True,
+    ) -> None:
+        # define the unique export path
+        if export_directory is None:
+            export_directory = os.getcwd()
+        if export_name is None:
+            export_name = "-".join([re.sub(" ", "_", str(x)) for x in ["Hillfit", "reg"]])
+
+        count = 0
+        export_path = os.path.join(export_directory, export_name)
+        while os.path.exists(export_path):
+            count += 1
+            export_name = re.sub("([0-9]+)$", str(count), export_name)
+            if not re.search("(-[0-9]+$)", export_name):
+                export_name += f"-{count}"
+            export_path = os.path.join(export_directory, export_name)
+        os.mkdir(export_path)
+
+        # export the figure
+        if figure:
+            self.figure.savefig(os.path.join(export_path, "regression.svg"))
+
+        # export the raw data
+        df = DataFrame(index=range(len(self.x_data)))
+        df["x"] = self.x_data
+        df["y"] = self.y_data
+        df.to_csv(os.path.join(export_path, "raw_data.csv"))
+
+        # export the fitted data
+        df2 = DataFrame(index=range(len(self.x_fit)))
+        df2["x_fit"] = self.x_fit
+        df2["y_fit"] = self.y_fit
+        df2.to_csv(os.path.join(export_path, "fitted_data.csv"))
+
+        # export the popt
+        arg_names = self._equation.__code__.co_varnames[:self._equation.__code__.co_argcount][1:]
+        string = "\n".join(
+            [
+                f"{arg_names[i]} = {val}" for i, val in enumerate(params)
+            ],
+        )
+        with open(os.path.join(export_path, "equation.txt"), "w") as output:
+            output.writelines(string)
